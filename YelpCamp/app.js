11d134494ffd75ea367744e1337b93e1b09831f1
yixuanzhou <zhouyixu@usc.edu>
2018-08-16 12:11:48 -0700

    YelpCamp v2

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,38 +1,60 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express    = require("express"),
+	app        = express(),
+	bodyParser = require("body-parser"),
+	mongoose   = require("mongoose");
 
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
+
+// SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	image : String,
+	description: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
 
 app.get("/", function(req, res){
 	res.render("landing")
 
 })
 
+// INDEX -- SHOW ALL CAMPGROUNDS
 app.get("/campgrounds", function(req, res){
-	var campgrounds = [
-		{name: "Salmon Creek", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTh1IhLxwuWb0uZ9xoeUsohFsB65TwtM318cLHxvZcGcnxqhKaQ"},
-	    {name: "Granite Hill", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6HUhwy8SjpufLpCxDAE-P3O_sls5BCywalrMCpLLF9k1ZoCYg9A"},
-	    {name: "Mountain Goat's Rest", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6sSrQeVBEsigi8ub-FuEjNus92buQ1M4j9feaIorI_vVIbuzK8g"}
-	]
-	res.render("campgrounds", {campgrounds:campgrounds});
-
+	Campground.find({}, function(err, campgrounds){
+		if(err) console.log(err);
+		else res.render("index", {campgrounds:campgrounds});
+	});
 });
 
+// CREATE -- ADD NEW CAMPGROUND TO DB
 app.post("/campgrounds", function(req, res){
 	// get data from form and add to campgrounds array
 	var name = req.body.name;
 	var image = req.body.image;
-	var newCampground = {name: name, image: image};
-	campgrounds.push(newCampground);
-	// redirect back to campgrounds page
-	res.redirect("/campgrounds");
+	var description = req.body.description;
+	var newCampground = {name: name, image: image, description:description };
+	// create new campground and save to db
+	Campground.create(newCampground, function(err, newCreated){
+		if (err) console.log(err);
+		else res.redirect("/campgrounds"); // redirect back to campgrounds page
+	});
 })
 
+// NEW -- SHOW FROM TO CREATE NEW CAMPGROUND
 app.get("/campgrounds/new", function(req, res){
 	res.render("new.ejs");
 })
+
+app.get("/campgrounds/:id", function(req,res){
+	Campround.FindById(req.params.id, function(err, foundCampground){
+		if (err) console.log(err);
+		else res.render("show", {campground: foundCampground});
+
+	});
+});
 
 app.listen(3000, function(){
 	console.log("The YelpCamp Server Has Started!");
